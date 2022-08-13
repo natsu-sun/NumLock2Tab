7bf0c901beee22149da116111b6b9fc2073faedb
natsu <38107965+natsu-sun@users.noreply.github.com>
2022-08-13 16:30:42 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <Windows.h>
+#include <stdio.h>
+
+int main()
+{
+    HKEY hKey;
+    DWORD dwDisposition;
+    LONG result;
+
+    result = RegCreateKeyEx(
+        HKEY_CURRENT_USER,
+        "System\\CurrentControlSet\\Control\\Keyboard Layout",
+        0, NULL,
+        //REG_OPTION_NON_VOLATILE, // 不揮発性
+        REG_OPTION_VOLATILE, // 揮発性 (再起動で消える)
+        KEY_ALL_ACCESS, NULL, &hKey, &dwDisposition);
+    printf("Open: %s\n", result == ERROR_SUCCESS ? "OK" : "NG");
+
+    unsigned char aData[0x15] = {
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x02, 0x00, 0x00, 0x00,
+        0x09, 0x00, 0x90, 0x00,
+        0x00, 0x00, 0x00, 0x00,
+    };
+
+    result = RegSetValueEx(hKey, "Scancode Map", 0, REG_BINARY, aData, sizeof(aData));
+    printf("Write: %s\n", result == ERROR_SUCCESS ? "OK" : "NG");
+
+    RegCloseKey(hKey);
+
+    return 0;
+}
